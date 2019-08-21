6703646dc0ebbf7f950c29f1f8cfaf63e454d792
jc_yao07 <jiacheng@pik-potsdam.de>
2019-08-21 10:26:46 +0200

    restructering

---
+++ revolut/I.b.sql
@@ -0,0 +1,13 @@
+﻿SELECT date_trunc('month', created_date::date) AS monthly, 
+	concat(10*floor(age/10), '-', 10*floor(age/10) + 10) AS age_group, 
+       sum(amount_usd) AS sum_amount, count(user_id) AS volume          
+FROM 
+(
+  select 
+    transactions.created_date, transactions.amount_usd, transactions.user_id, 
+    (date_part('year', CURRENT_DATE) - users.birth_year) AS age
+  from 
+    transactions inner join users on transactions.user_id = users.user_id where transactions.transactions_type = 'CARD_PAYMENT'
+) AS t
+GROUP BY monthly, age_group
+
---
+++ revolut/I.c.sql
@@ -0,0 +1,23 @@
+﻿SELECT 
+    sum(CASE WHEN merged_pivot_table.Nonstandard < merged_pivot_table.Standard THEN 1 else 0 END) as Number_of_Days_Nonstandard_is_Less,
+    sum(CASE WHEN merged_pivot_table.Nonstandard >= merged_pivot_table.Standard THEN 1 else 0 END) as Number_of_Days_Nonstandard_is_More
+FROM
+   (SELECT * 
+FROM crosstab( 
+'SELECT cast (transaction_date AS text), plan, count_transactions FROM (
+SELECT merged_table.plan, merged_table.transaction_date, 
+count(merged_table.count_transactions) as count_transactions
+ FROM (
+SELECT
+   users.user_id, CASE WHEN users.plan = ''STANDARD'' THEN ''STANDARD'' ELSE ''NONSTANDARD'' END as plan, transactions_agg.count_transactions, transactions_agg.transaction_date
+FROM
+   users
+INNER JOIN 
+(SELECT transactions.user_id, date(transactions.created_date) as transaction_date, 
+	count(transactions.transaction_id) as count_transactions
+	FROM  transactions group by transactions.user_id, date(transactions.created_date) 
+) transactions_agg 
+ON users.user_id = transactions_agg.user_id) merged_table
+GROUP BY merged_table.plan, merged_table.transaction_date) merged_final_table order by 1,2'
+) 
+AS final_result(Transaction_Date TEXT, Standard bigint, Nonstandard bigint)) merged_pivot_table
