656c8ef71b1523b1ee3249b5cb903e68fa696432
jiacheng-yao <jc07.yao@gmail.com>
2021-02-11 23:45:34 +0100

    solutions for sr

---
+++ simplyrational/r_fftrees.R
@@ -0,0 +1,31 @@
+library("FFTrees")
+library("yardstick")
+df_final <- read.csv("~/Codes/simplyrational/app_final(w_target).csv", header = TRUE)
+df_final$TARGET <- as.logical(df_final$TARGET)
+
+#Create 10 equally size folds
+folds <- cut(seq(1,nrow(df_final)),breaks=10,labels=FALSE)
+
+#Perform 10 fold cross validation
+bacc_agg <- 0
+for(i in 1:10){
+  #Segement your data by fold using the which() function 
+  testIndexes <- which(folds==i,arr.ind=TRUE)
+  testData <- df_final[testIndexes, ]
+  trainData <- df_final[-testIndexes, ]
+  #Use the test and train data partitions however you desire...
+  
+  credit_FFT <- FFTrees(formula = TARGET ~ .,               # The variable we are predicting
+                        data = trainData,                    # Training data
+                        data.test = testData,                # Testing data
+                        main = "Credit Decision",                # Main label
+                        decision.labels = c("Others", "With Difficulties"), 
+                        do.comp = FALSE) # Label for decisions
+  
+  pred_label <- predict(credit_FFT, testData)
+  
+  truth_factor <- factor(testData$TARGET, labels=c("Others", "With Difficulties"))
+  pred_factor <- factor(pred_label, labels=c("Others", "With Difficulties"))
+  bacc_agg <- bacc_agg + bal_accuracy_vec(truth_factor, pred_factor)
+}
+bacc_agg <- bacc_agg/10.0
